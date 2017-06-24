9e0200b000107df99531a3210d97b296c939ec77
Leopold Shim <skywoo1994@likelion.org>
2017-06-24 11:55:49 +0000

    delete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,4 +39,7 @@ module SessionsHelper
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
     end
+    def current_user?(user)
+    user == current_user
+    end
 end
